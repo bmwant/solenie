b939040438c2e41d71355de33ddecbf6c395cb01
Misha Behersky <bmwant@gmail.com>
2018-10-20 23:14:17 +0300

    Generate cloud for pos/reg/bad :fist:

--- summer/stats.py
+++ summer/stats.py
@@ -3,16 +3,24 @@ from wordcloud import WordCloud
 from tinydb import TinyDB, Query
 
 import settings
+from buttworld.logger import get_logger
+from jerry.parser.review import SentimentEnum
 
 
 db = TinyDB(settings.TOP_500_MOVIE_REVIEWS)
+logger = get_logger(__name__)
 
 
-def main():
+def get_reviews_by_sentiment(sentiment):
     query = Query()
-    results = db.search(query.text.exists())[:10]
-    text = results[0]['text']
+    return db.search(query.sentiment == sentiment)
 
+
+def get_text_for_reviews(reviews):
+    return ' '.join([r['text'] for r in reviews])
+
+
+def generate_cloud_for_text(text):
     # Generate a word cloud image
     wordcloud = WordCloud().generate(text)
 
@@ -24,8 +32,22 @@ def main():
     plt.show()
 
     # The pil way (if you don't have matplotlib)
-    image = wordcloud.to_image()
-    image.show()
+    # image = wordcloud.to_image()
+    # image.show()
+
+
+def main():
+    pos_reviews = get_reviews_by_sentiment(SentimentEnum.GOOD)
+    reg_reviews = get_reviews_by_sentiment(SentimentEnum.NEUTRAL)
+    bad_reviews = get_reviews_by_sentiment(SentimentEnum.BAD)
+
+    pos_text = get_text_for_reviews(pos_reviews)
+    reg_text = get_text_for_reviews(reg_reviews)
+    bad_text = get_text_for_reviews(bad_reviews)
+
+    generate_cloud_for_text(pos_text)
+    generate_cloud_for_text(reg_text)
+    generate_cloud_for_text(bad_text)
 
 
 if __name__ == '__main__':
