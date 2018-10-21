eaccc6a6213135e115a3bcf2ed1e57c4e7c84fd5
Misha Behersky <bmwant@gmail.com>
2018-10-21 22:18:47 +0300

    Generate clouds for sentiments :small_red_triangle:

--- summer/stats.py
+++ summer/stats.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from nltk.probability import FreqDist
+from nltk.tokenize import sent_tokenize, word_tokenize
 from tinydb import TinyDB, Query
 
 import settings
@@ -13,41 +15,63 @@ logger = get_logger(__name__)
 
 def get_reviews_by_sentiment(sentiment):
     query = Query()
-    return db.search(query.sentiment == sentiment)
+    result = db.search(query.sentiment == sentiment)
+    logger.info('Loaded %s records for %s sentiment', len(result), sentiment)
+    return result
+
+
+def tokenize(text):
+    words = []
+    for sentence in sent_tokenize(text):
+        for word in word_tokenize(sentence):
+            words.append(word)
+    return words
+
+
+def show_stats_for_text(text):
+    words = [w.lower() for w in tokenize(text)]
+    fd = FreqDist(words)
+    logger.info('Total words: %s', len(words))
+    logger.info('Recurrent words: %s', fd.B())
+    logger.info('Most common words')
+    for word, count in fd.most_common(20):
+        logger.info('%s\t%s', word, count)
 
 
 def get_text_for_reviews(reviews):
-    return ' '.join([r['text'] for r in reviews])
+    text = ' '.join([r['text'] for r in reviews])
+    show_stats_for_text(text)
+    return text
 
 
-def generate_cloud_for_text(text):
-    # Generate a word cloud image
-    wordcloud = WordCloud().generate(text)
-
-    # lower max_font_size
+# todo: todo hotkey
+# todo: image on the whole canvas
+def generate_cloud_for_text(text, name='file'):
     wordcloud = WordCloud(max_font_size=40).generate(text)
-    plt.figure()
+    fig = plt.figure()
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
-
+    fig.savefig(f'{name}.png')  # , dpi=fig.dpi)
     # The pil way (if you don't have matplotlib)
     # image = wordcloud.to_image()
     # image.show()
 
 
+# todo: stopwords
+# todo: punctuations, check simple example
+# ~/nltk_data/tokenizers/punkt
+# https://stackoverflow.com/questions/21160310/training-data-format-for-nltk-punkt
 def main():
-    pos_reviews = get_reviews_by_sentiment(SentimentEnum.GOOD)
-    reg_reviews = get_reviews_by_sentiment(SentimentEnum.NEUTRAL)
-    bad_reviews = get_reviews_by_sentiment(SentimentEnum.BAD)
-
-    pos_text = get_text_for_reviews(pos_reviews)
-    reg_text = get_text_for_reviews(reg_reviews)
-    bad_text = get_text_for_reviews(bad_reviews)
-
-    generate_cloud_for_text(pos_text)
-    generate_cloud_for_text(reg_text)
-    generate_cloud_for_text(bad_text)
+    sentiments = (
+        SentimentEnum.GOOD,
+        SentimentEnum.NEUTRAL,
+        SentimentEnum.BAD,
+    )
+    for sentiment in sentiments:
+        reviews = get_reviews_by_sentiment(sentiment)
+        text = get_text_for_reviews(reviews)
+        generate_cloud_for_text(text, name=sentiment.name.lower())
 
 
 if __name__ == '__main__':
