fcfc32e9711c58a60f1b655f449fbb785d490ea1
Misha Behersky <bmwant@gmail.com>
2018-10-25 12:54:18 +0300

    Make counter for partitioner :rooster:

--- summer/partitioner/distribution.py
+++ summer/partitioner/distribution.py
@@ -3,6 +3,11 @@ import math
 from buttworld.logger import get_logger
 
 
+def cnt(steps):
+    it = iter(range(1, steps+1))
+    return lambda: next(it, False)
+
+
 class DistributionPartitioner(object):
     def __init__(self, data, pred, ratio=0.85):
         self.data = data
@@ -12,30 +17,26 @@ class DistributionPartitioner(object):
         self._test_data = None
         self.logger = get_logger(self.__class__.__name__.lower())
 
-    # todo (misha): write it in a good way please
     def partition(self):
+        # todo (misha): reuse it??!
         len_pos = len(list(filter(self.pred, self.data)))
         len_neg = len(self.data) - len_pos
         len_pos_training = math.floor(len_pos*self.ratio)
         len_neg_training = math.floor(len_neg*self.ratio)
 
-        pos_ = 0
-        neg_ = 0
+        go_pos = cnt(len_pos_training)
+        go_neg = cnt(len_neg_training)
+
         partition_training = []
         partition_test = []
+        def cond(item):
+            # WARNING: it heavily relies on lazy evaluation, be careful
+            return self.pred(item) and go_pos() or \
+                   not self.pred(item) and go_neg()
+
         for item in self.data:
-            if self.pred(item):
-                if pos_ < len_pos_training:
-                    partition_training.append(item)
-                    pos_ += 1
-                else:
-                    partition_test.append(item)
-            else:
-                if neg_ < len_neg_training:
-                    partition_training.append(item)
-                    neg_ += 1
-                else:
-                    partition_test.append(item)
+            partition_training.append(item) \
+                if cond(item) else partition_test.append(item)
 
         self.logger.info(
             'Created train partition(%s) and test partition(%s)',
