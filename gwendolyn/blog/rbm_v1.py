d30246f8bf0acee5e8518d4572dfcdac3b81077d
Misha Behersky <bmwant@gmail.com>
2019-02-01 09:16:59 +0200

    rbbmm v1 :white_flower:

--- gwendolyn/blog/rbm_v1.py
+++ gwendolyn/blog/rbm_v1.py
@@ -1,4 +1,5 @@
 """
+Adapted from http://deeplearning.net/tutorial/rbm.html
 python 3.6.5
 http://deeplearning.net/software/theano/install_ubuntu.html
 """
@@ -103,3 +104,170 @@ class RBM(object):
         pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v1_sample)
         return [pre_sigmoid_v1, v1_mean, v1_sample,
                 pre_sigmoid_h1, h1_mean, h1_mean]
+
+    def free_energy(self, v_sample):
+        wx_b = T.dot(v_sample, self.W) + self.hbias
+        vbias_term = T.dot(v_sample, self.vbias)
+        hidden_term = T.sum(T.log(1 + T.exp(wx_b)), axis=1)
+        return -hidden_term - vbias_term
+
+    def get_cost_updates(self, lr=0.1, persistent=None, k=1):
+        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(self.input_layer)
+        if persistent is None:
+            chain_start = ph_sample
+        else:
+            chain_start = persistent
+
+        (
+            [
+                pre_sigmoid_nvs,
+                nv_means,
+                nv_samples,
+                pre_sigmoid_nhs,
+                nh_means,
+                nh_samples,
+            ],
+            updates
+        ) = theano.scan(
+            self.gibbs_hvh,
+            outputs_info=[None, None, None, None, None, chain_start],
+            n_steps=k,
+            name='gibbs_hvh',
+        )
+
+        chain_end = nv_samples[-1]
+
+        cost = T.mean(self.free_energy(self.input_layer)) - T.mean(
+            self.free_energy(chain_end)
+        )
+
+        gparams = T.grad(cost, self.params, consider_constant=[chain_end])
+
+        for gparam, param in zip(gparams, self.params):
+            updates[param] = param - gparam * T.cast(
+                lr,
+                dtype=theano.config.floatX
+            )
+
+        if persistent:
+            updates[persistent] = nh_samples[-1]
+            monitoring_cost = self.get_pseudo_likelihood_cost(updates)
+        else:
+            monitoring_cost = self.get_reconstruction_cost(updates, pre_sigmoid_nvs[-1])
+
+        return monitoring_cost, updates
+
+    def get_pseudo_likelihood_cost(self, updates):
+        bit_i_idx = theano.shared(value=0, name='bit_i_idx')
+
+        xi = T.round(self.input_layer)
+        fe_xi = self.free_energy(xi)
+
+        xi_flip = T.set_subtensor(xi[:, bit_i_idx], 1 - xi[:, bit_i_idx])
+        fe_xi_flip = self.free_energy(xi_flip)
+
+        cost = T.mean(self.n_visible * T.log(T.nnet.sigmoid(fe_xi_flip - fe_xi)))
+
+        updates[bit_i_idx] = (bit_i_idx + 1) % self.n_visible
+        return cost
+
+
+def train_rmb():
+    train_rbm = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size]
+        },
+        name='train_rbm'
+    )
+
+    plotting_time = 0.
+    start_time = timeit.default_timer()
+
+    for epoch in range(training_epochs):
+        mean_cost = []
+        for batch_index in range(n_train_batches):
+            mean_cost += [train_rbm(batch_index)]
+
+        print('Training epoch %d, cost is ' % epoch, np.mean(mean_cost))
+
+        plotting_start = timeit.default_timer()
+        image = Image.fromarray(
+            tile_raster_images(
+                X=rbm.W.get_value(borrow=True).T,
+                img_shape=(28, 28),
+                tile_shape=(10, 10),
+                tile_spacing=(1, 1),
+            )
+        )
+        image.save('filters_at_epoch_%i.png' % epoch)
+        plotting_stop = timeit.default_timer()
+        plotting_time += (plotting_stop - plotting_start)
+
+    end_time = timeit.default_timer()
+
+    pretraining_time = (end_time - start_time) - plotting_time
+    print('Training took %f minutes' % (pretraining_time / 60.))
+
+
+    number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
+
+    test_idx = rng.randint(number_of_test_samples - n_chains)
+    persistent_vis_chain = theano.shared(
+        np.asarray(
+            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+            dtype=theano.config.floatX,
+        )
+    )
+
+
+    plot_every = 1000
+    (
+        [
+            presig_hids,
+            hid_mfs,
+            hid_samples,
+            presig_vis,
+            vis_mfs,
+            vis_samples,
+        ],
+        updates
+    ) = theano.scan(
+        rbm.gibbs_vhv,
+        outputs_info=[None, None, None, None, None, persistent_vis_chain],
+        n_steps=plot_every,
+        name='gibbs_vhv',
+    )
+
+    updates.update({persistent_vis_chain: vis_samples[-1]})
+    sample_fn = theano.function(
+        [],
+        [
+            vis_mfs[-1],
+            vis_samples[-1],
+        ],
+        updates=updates,
+        name='sample_fn',
+    )
+
+    image_data = np.zeros(
+        (29 * n_samples + 1, 29 * n_chains - 1),
+        dtype='uint8',
+    )
+
+    for idx in range(n_samples):
+        vis_mf, vis_sample = sample_fn()
+        print(' ... plotting sample %d' % idx)
+        image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+            X=vis_mf,
+            img_shape=(28, 28),
+            tile_shape=(1, n_chains),
+            tile_spacing=(1, 1),
+        )
+
+    image = Image.fromarray(image_data)
+    image.save('samples.png')
+
+
